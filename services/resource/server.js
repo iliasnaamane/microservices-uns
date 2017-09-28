b44582a77adf13d638bf3869e99e69c871578c5b
iliasnaamane <ilias.naamane1@gmail.com>
2017-09-28 04:46:46 +0200

    docker environment

--- services/resource/server.js
+++ services/resource/server.js
@@ -4,10 +4,12 @@ port = process.env.PORT || 3000,
 mongoose = require('mongoose'),
 BusinessTravel = require('./api/models/BusinessTravel'),
 bodyParser = require('body-parser');
+config = require('./config');
 
 //mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/BusinessTravelDB',{ useMongoClient: true });
+console.log(config.db[app.settings.env]);
+mongoose.connect(config.db[app.settings.env],{ useMongoClient: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
