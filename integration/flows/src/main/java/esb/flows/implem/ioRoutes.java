fe6c8a7fd45c566da022f28f90368b43a840822d
Unknown <maxime.dejeux@gmail.com>
2017-10-23 01:21:08 +0200

    Car rental route
    Ma residence bloque le download de service mix. Donc version non testé

--- integration/flows/src/main/java/esb/flows/implem/ioRoutes.java
+++ integration/flows/src/main/java/esb/flows/implem/ioRoutes.java
@@ -5,7 +5,7 @@
  */
 package esb.flows.implem;
 
-import esb.flows.implem.data.HotelSpec;
+import esb.flows.implem.data.*;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,6 +18,7 @@ import esb.flows.implem.utils.HotelSearchHelper;
 import java.util.ArrayList;
 import stubs.hotel_rpc.Hotel;
 import stubs.hotel_rpc.HotelFinderService;
+import esb.flows.implem.utils.CarRentalHelper;
 
 /**
  *
@@ -55,18 +56,43 @@ public class ioRoutes extends RouteBuilder {
                 .process(HotelSearchHelper.RequestRPC)
                 .to(LETTER_OUTPUT_DIR+"?fileName=cheapHotel.txt")
         ;
+
+
+        /*------------------Add Car request-------------------*/
+         from(CSV_INPUT_FILE_CAR)
+            .routeId("csv-input-car")
+            .routeDescription("Loads a CSV containg request")
+            .log("Processing ${file:name}")
+            .unmarshal(CsvFormat.buildCsvFormat())
+            .log("Loading the file as a CSV document")
+            .split(body())
+                .parallelProcessing().executorService(WORKERS)
+                .process(csv2Request)
+            .to(SEARCH_CAR);
+        
+        from(SEARCH_CAR)
+            .routeId("search-car")
+            .routeDescription("Call the car rental service")
+            .log("Processing ${file:name}")
+            .bean(CarRentalHelper.class, "buildGetCarsRequest(${body})")
+            .inOut(CAR_RPC_ENDPOINT)
+            .log("THIS IS SPARTA")
+                ;
                 
        
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    private static   Processor csv2Request = (Exchange exchange)-> {
+    System.out.println("csv2Request");
+    Map<String, Object> data = (Map<String, Object>) exchange.getIn().getBody();
+    Request req = new Request();
+    req.setPlace((String)data.get("place"));
+    req.setDateStart((String)data.get("dateStart"));
+    req.setDateEnd((String)data.get("dateEnd"));
+    System.out.println(req.toString());
+    exchange.getIn().setBody(req);
+    System.out.println("Fin csv2Request");
+    };
     
    /* private static   Processor listToCheapHotel = (Exchange exchange)-> {
         System.out.println("listHotelsToCheapHotel");
--- integration/flows/src/main/java/esb/flows/implem/utils/Endpoints.java
+++ integration/flows/src/main/java/esb/flows/implem/utils/Endpoints.java
@@ -4,6 +4,8 @@ public class Endpoints {
     // file inputs
     public static final String CSV_INPUT_FILE_HOTEL = "file:/Users/iliasnaamane/microservices-uns/integration/input"
             + "?fileName=hotels.csv" ;
+     public static final String CSV_INPUT_FILE_CAR = "file:/Users/iliasnaamane/microservices-uns/integration/input"
+            + "?fileName=car.csv" ;
     
     // file outputs
     public static final String LETTER_OUTPUT_DIR = "file:/Users/iliasnaamane/microservices-uns/integration/output";
@@ -14,9 +16,12 @@ public class Endpoints {
     // direct endpoints
     public static final String SEARCH_HOTEL_1 = "direct:search-hotel-1";
     public static final String SEARCH_HOTEL_2 = "direct:search-hotel-2";
-    
+    public static final String SEARCH_CAR = "direct:search-car";
+
     //services endpoints
     public static final String HOTEL_RPC_ENDPOINT = "http://localhost:9010/hotel-rpc/ExternalHotelFinderService";
+    public static final String CAR_RPC_ENDPOINT = "http:car-rpc:8080/car-rpc/ExternalCarRentalService";
+
     //public static final String HOTEL_RPC_WSDL_URL = "http://localhost:9010/hotel-rpc/ExternalHotelFinderService?wsdl&";
     //public static final String HOTEL_SERVICE_NAME = "ExternalHotelFinderService";
     //public static final String PORT_NAME = "ExternalHotelFinderPort";
