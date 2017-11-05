ccf06fb3b5ed9cfe59b640f7d0b152a32bb46968
iliasnaamane <ilias.naamane1@gmail.com>
2017-11-05 04:08:58 +0100

    remove business travel node_modules

--- cp-files.sh
+++ cp-files.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-<<<<<<< HEAD
+#cp hotels.csv integration/input
+#cp cars.csv integration/input
+#cp vol.csv integration/input
+
 cp hotels.csv integration/input
 #cp cars.csv integration/input
 #cp vol.csv integration/input
-=======
-#cp hotels.csv integration/input
-#cp cars.csv integration/input
-cp vol.csv integration/input
->>>>>>> d380edad648d519bce4e77b1dece023d1bb03c0e
--- deploy-servicemix.sh
+++ deploy-servicemix.sh
@@ -4,9 +4,3 @@ cd integration
 mvn clean package
 #rm ../../apache-servicemix-7.0.1/deploy/implem-1.0.jar
 cp flows/target/implem-1.0.jar ../../apache-servicemix-7.0.1/deploy/.
-
-<<<<<<< HEAD
-=======
-#cd ../apache-servicemix-7.0.1/bin/
->>>>>>> d380edad648d519bce4e77b1dece023d1bb03c0e
-#./servicemix
